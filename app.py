4310e2e43c78d7df63a1404d37b4a04494d1b175
m3tasploit <abdulmuhaiminmunu@gmail.com>
2021-03-01 23:24:47 +0530

    recommendations working

--- app.py
+++ app.py
@@ -1,45 +1,70 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, Response
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv("./data.csv")
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import json
+
+
+tfv = TfidfVectorizer(min_df=3,  max_features=None, 
+            strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}',
+            ngram_range=(1, 3),
+            stop_words = 'english')
+
+# Filling NaNs with empty string
+dataset['Summary'] = dataset['Summary'].fillna('')
+
+tfv_matrix = tfv.fit_transform(dataset['Summary'])
+
+from sklearn.metrics.pairwise import sigmoid_kernel
+
+# Compute the sigmoid kernel
+sig = sigmoid_kernel(tfv_matrix, tfv_matrix)
+indices = pd.Series(dataset.index, index=dataset['Title']).drop_duplicates()
+
+def give_rec(title, sig=sig):
+    # Get the index corresponding to original_title
+    idx = indices[title]
+
+    # Get the pairwsie similarity scores 
+    sig_scores = list(enumerate(sig[idx]))
+
+    # Sort the movies 
+    sig_scores = sorted(sig_scores, key=lambda x: x[1], reverse=True)
+
+    # Scores of the 10 most similar movies
+    sig_scores = sig_scores[1:11]
+
+    # Movie indices
+    movie_indices = [i[0] for i in sig_scores]
+
+    # Top 10 most similar movies
+    return dataset['Title'].iloc[movie_indices]
+
+@app.route('/getrecs/', methods=['GET'])
 def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+    # Retrieve the title from url parameter
+    title = request.args.get("title", None)
 
     # For debugging
-    print(f"got name {name}")
+    print(f"got title {title}")
 
     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
+    if not title:
+        response["ERROR"] = "no title found, please send a title."
+    elif str(title).isdigit():
+        response["ERROR"] = "title can't be numeric."
     else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+        response["recs"] = give_rec(title).to_json(orient="records")
 
     # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+    return Response(json.dumps(response), mimetype='application/json')
 
 # A welcome message to test our server
 @app.route('/')
